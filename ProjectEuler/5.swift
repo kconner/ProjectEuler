4fd2fdc4a8123683c35e9b5c17ebf4eb11e648ba
Kevin Conner <connerk@gmail.com>
2014-06-08 05:21:52 -0400

    Solved problem 5.

---
+++ ProjectEuler/5.swift
@@ -0,0 +1,36 @@
+//
+//  5.swift
+//  ProjectEuler
+//
+//  Created by Kevin Conner on 6/8/14.
+//  Copyright (c) 2014 Kevin Conner. All rights reserved.
+//
+
+import Foundation
+
+func p5() -> Int {
+    struct PrimeFactor {
+        let prime: Int
+        var power: Int
+    }
+    
+    let primes = [2, 3, 5, 7, 11, 13, 17, 19]
+    let powers = [0, 0, 0, 0, 0, 0, 0, 0]
+    
+    for value in 2..20 {
+        for index in primes.startIndex..primes.endIndex {
+            let prime = primes[index]
+            var dividend = value
+            var power = 0
+            while dividend % prime == 0 {
+                dividend /= prime
+                ++power
+            }
+            powers[index] = max(powers[index], power)
+        }
+    }
+    
+    return reduce(Zip2(primes, powers), 1, { product, factor in
+        return product * Int(pow(CDouble(factor.0), CDouble(factor.1)))
+    })
+}
--- ProjectEuler/main.swift
+++ ProjectEuler/main.swift
@@ -10,7 +10,7 @@
 
 import Cocoa
 
-let problems = [p1, p2, p3, p4]
+let problems = [p1, p2, p3, p4, p5]
 
 var problemNumber = problems.count
 
