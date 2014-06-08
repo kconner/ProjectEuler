96b1b8ea2e176b75cc93b69ba57fa8573b0f86a1
Kevin Conner <connerk@gmail.com>
2014-06-08 00:36:14 -0400

    Initial Commit

---
+++ ProjectEuler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProjectEuler
+//
+//  Created by Kevin Conner on 6/8/14.
+//  Copyright (c) 2014 Kevin Conner. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
