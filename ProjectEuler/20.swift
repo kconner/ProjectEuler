771ba040e70806df4a2bc732e2313fb98aa03426
Kevin Conner <connerk@gmail.com>
2014-11-09 00:33:06 -0500

    Solved problem 20.

---
+++ ProjectEuler/20.swift
@@ -0,0 +1,16 @@
+//
+//  20.swift
+//  ProjectEuler
+//
+//  Created by Kevin Conner on 11/9/14.
+//  Copyright (c) 2014 Kevin Conner. All rights reserved.
+//
+
+func p20() -> Int {
+    var decimal = BigDecimal(1)
+    for i in 2...100 {
+        decimal = decimal.multipliedBy(i)
+    }
+
+    return decimal.digits.reduce(0, +)
+}
