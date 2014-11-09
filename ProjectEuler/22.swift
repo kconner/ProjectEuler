df5fc39a7d8faf0dde2f73c68aa696704943b04e
Kevin Conner <connerk@gmail.com>
2014-11-09 01:03:12 -0500

    Solved problem 22.

---
+++ ProjectEuler/22.swift
@@ -0,0 +1,36 @@
+//
+//  22.swift
+//  ProjectEuler
+//
+//  Created by Kevin Conner on 11/9/14.
+//  Copyright (c) 2014 Kevin Conner. All rights reserved.
+//
+
+import Foundation
+
+private func parseNames(string: String) -> [String] {
+    return string.componentsSeparatedByString(",").map {
+        $0.componentsSeparatedByString("\"")[1]
+    }
+}
+
+private func letterValueForName(name: String) -> Int {
+    let basis = UnicodeScalar("A").value - 1
+    var sum = 0
+    for scalar in name.unicodeScalars {
+        sum += scalar.value - basis
+    }
+    return sum
+}
+
+func p22() -> Int {
+    if let path = NSBundle.mainBundle().pathForResource("names", ofType: "txt") {
+        if let string = NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil) {
+            let sortedNames = parseNames(string).sorted(<)
+            var index = 1
+            return sortedNames.map { letterValueForName($0) * index++ }.reduce(0, +)
+        }
+    }
+
+    return -1
+}
